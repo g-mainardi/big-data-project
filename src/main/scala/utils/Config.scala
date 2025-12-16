f8835d6a6dc84e3b0990a5c919b347726e33ccfd
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-12-16 17:26:29 +0100

    chore: import project configuration files

---
+++ src/main/scala/utils/Config.scala
@@ -0,0 +1,14 @@
+package utils
+
+object Config {
+
+  // The local directory containing this repository
+  val projectDir :String = "/C:/Users/giosy/BIGDATA/lab-cse-25-26"
+  // The name of the shared bucket on AWS S3 to read datasets (so you don't need to upload them in your bucket)
+  val s3sharedBucketName :String = "unibo-bd2526-egallinucci-shared"
+  // The name of your bucket on AWS S3
+  val s3bucketName :String = "unibo-bd2526-giosue"
+  // The path to the credentials file for AWS (if you follow instructions, this should not be updated)
+  val credentialsPath :String = "/aws_credentials.txt"
+
+}
